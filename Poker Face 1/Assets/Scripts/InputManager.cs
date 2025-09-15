94f38bc923522a3681a6c71f10527343c3e6129b
RandomStudios1440 <sowdennicolas@gmail.com>
2025-09-15 10:46:54 -0400

    geo0gop

--- Poker Face 1/Assets/Scripts/InputManager.cs
+++ Poker Face 1/Assets/Scripts/InputManager.cs
@@ -6,17 +6,21 @@ public class InputManager : MonoBehaviour
 {
     private PlayerInput playerInput;
     private PlayerInput.OnFootActions OnFoot;
+
+    private PlayerMotor motor;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
         playerInput = new PlayerInput();
         OnFoot = playerInput.OnFoot;
+        motor = GetComponent<PlayerMotor>();
     }
 
         // Update is called once per frame
-        void Update()
+        void FixedUpdate()
         {
-
+        //tell the playermotor to move using the value from our movement action.
+        motor.ProcessMove(OnFoot.Movement.ReadValue<Vector2>());
         }
     private void OnEnable()
     {
