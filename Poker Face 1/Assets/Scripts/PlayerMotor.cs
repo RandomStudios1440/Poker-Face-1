2cc48da534b7128bd6bfa8fd9247710a3e40ba63
RandomStudios1440 <sowdennicolas@gmail.com>
2025-09-17 07:34:02 -0400

    Update PlayerMotor.cs

--- Poker Face 1/Assets/Scripts/PlayerMotor.cs
+++ Poker Face 1/Assets/Scripts/PlayerMotor.cs
@@ -2,15 +2,25 @@ using UnityEngine;
 
 public class PlayerMotor : MonoBehaviour
 {
+    private CharacterController controller;
+    private Vector3 playervelocity;
+    public float speed = 5f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public void ProcessMove(Vector2 input)
+    {
+        Vector3 moveDirection = Vector3.zero;
+        moveDirection.x = input.x;
+        moveDirection.y = input.y;
+        controller.Move(transform.TransformDirection(moveDirection) * speed * Time.deltaTime);
     }
 }
