eb44d4cb4c866b9938684e37007130744f5d19fa
RandomStudios1440 <sowdennicolas@gmail.com>
2025-10-03 09:53:41 -0400

    kopkoyh

--- Poker Face 1/Assets/Scripts/MainMenu.cs
+++ Poker Face 1/Assets/Scripts/MainMenu.cs
@@ -7,6 +7,10 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadSceneAsync(1);
     }
+    public void Store()
+    {
+        SceneManager.LoadSceneAsync(2);
+    }
     public void QuitGame()
     {
         Application.Quit();
