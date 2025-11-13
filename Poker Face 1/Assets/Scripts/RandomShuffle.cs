65c5a0a46a1364a933d0b91a10d7baebbcb15cde
CDREDsauce <cameronward09092009@gmail.com>
2025-11-13 09:31:30 -0500

    Mr. Smith Fixes

--- Poker Face 1/Assets/Scripts/RandomShuffle.cs
+++ Poker Face 1/Assets/Scripts/RandomShuffle.cs
@@ -1,16 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class RandomGenerator : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public List<int> shuffleList = new List<int>();
+    public int listRange;
+    public int shuffletimes;
+    private void Start()
     {
-        
+        for (int i = 0; i < listRange; i++)
+        {
+            shuffleList.Add(i + 1);
+        }
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ShuffleListRandomly();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void ShuffleListRandomly()
     {
-        
+        for(int i = 0;i < shuffleList.Count;i++)
+        {
+            for (int s= 0; i < shuffleList.Count; s++)
+            {
+                int r = (int)(Random.value * (shuffleList[i] - s));
+                int tempValue = shuffleList[r];
+                shuffleList[r] = shuffleList[s];
+                shuffleList[s] = tempValue;
+            }
+        }
     }
 }
+ 
