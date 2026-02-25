575ba5d6a4753cc9e9729f253629ab16ce99c34c
RandomStudios1440 <sowdennicolas@gmail.com>
2026-02-25 04:36:51 -0500

    Revert "revert this"
    This reverts commit d5951f7579703289f7d9f34dcd6145bbeac2398e.

---
+++ Poker Face 1/Assets/Scripts/Card.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    public enum Suit { Hearts, Diamonds, Clubs, Spades }
+    public enum Rank { Two = 2, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Eleven, Queen, King, Ace }
+    
+    public Suit suit;
+    public Rank rank;
+    public bool isFaceUp = false;
+    
+    public void Initialize(Suit cardSuit, Rank cardRank)
+    {
+        suit = cardSuit;
+        rank = cardRank;
+    }
+    
+    public void Flip()
+    {
+        isFaceUp = !isFaceUp;
+        // Rotate card to show face or back
+        transform.rotation = isFaceUp ? Quaternion.Euler(0, 0, 0) : Quaternion.Euler(0, 180, 0);
+    }
+    
+    public int GetValue()
+    {
+        return (int)rank;
+    }
+    
+    public override string ToString()
+    {
+        return $"{rank} of {suit}";
+    }
+}
