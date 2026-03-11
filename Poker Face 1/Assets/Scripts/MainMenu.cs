bb21f2f446f3c06a4b29701c341d06c10ec3ee32
RandomStudios1440 <sowdennicolas@gmail.com>
2026-03-11 08:54:55 -0400

    mnbbnmmb

--- Poker Face 1/Assets/Scripts/MainMenu.cs
+++ Poker Face 1/Assets/Scripts/MainMenu.cs
@@ -6,7 +6,7 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene(1);
+        SceneTransition.Instance.LoadScene(1);
     }
     public void Store()
     {
