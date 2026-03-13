d9fd7e56ace2127e31004ae186048b488c166157
RandomStudios1440 <sowdennicolas@gmail.com>
2026-03-13 08:24:27 -0400

    cvbvcvc

--- Poker Face 1/Assets/Scripts/MainMenu.cs
+++ Poker Face 1/Assets/Scripts/MainMenu.cs
@@ -6,17 +6,11 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        if (SceneTransition.Instance != null)
-            SceneTransition.Instance.LoadScene(1);
-        else
-            SceneManager.LoadScene(1);
+        SceneTransition.Instance.LoadScene(1);
     }
     public void Store()
     {
-        if (SceneTransition.Instance != null)
-            SceneTransition.Instance.LoadScene(2);
-        else
-            SceneManager.LoadScene(2);
+        SceneTransition.Instance.LoadScene(2);
     }
     public void QuitGame()
     {
@@ -24,30 +18,18 @@ public class MainMenu : MonoBehaviour
     }
     public void Menu()
     {
-        if (SceneTransition.Instance != null)
-            SceneTransition.Instance.LoadScene(0);
-        else
-            SceneManager.LoadScene(0);
+        SceneTransition.Instance.LoadScene(0);
     }
     public void Options()
     {
-        if (SceneTransition.Instance != null)
-            SceneTransition.Instance.LoadScene(3);
-        else
-            SceneManager.LoadScene(3);
+        SceneTransition.Instance.LoadScene(3);
     }
     public void Play()
     {
-        if (SceneTransition.Instance != null)
-            SceneTransition.Instance.LoadScene(4);
-        else
-            SceneManager.LoadScene(4);
+        SceneTransition.Instance.LoadScene(4);
     }
     public void Begin()
     {
-        if (SceneTransition.Instance != null)
-            SceneTransition.Instance.LoadScene(5);
-        else
-            SceneManager.LoadScene(5);
+        SceneTransition.Instance.LoadScene(5);
     }
 }
