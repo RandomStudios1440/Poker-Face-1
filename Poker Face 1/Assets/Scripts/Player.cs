bfa365371efc0e06b4ecc2126e77cede4f1c2f36
RandomStudios1440 <sowdennicolas@gmail.com>
2026-04-01 09:41:40 -0400

    sdsdaadsdas

---
+++ Poker Face 1/Assets/Scripts/Player.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using UnityEngine.XR;
+
+public class Player : MonoBehaviour
+{
+    public string Name { get; }
+    public Hand Hand { get; private set; }
+    public int Chips { get; private set; }
+    public bool HasFolded { get; private set; }
+
+    public Player (string name, int startingChips = 1000)
+    {
+        Name = name;
+        Chips = startingChips;
+        Hand = new Hand();
+        HasFolded = false;
+    }
+
+    public void DealCard(Card card)
+    {
+        Hand.AddCard(card);
+    }
+
+    public void Bet(int amount)
+    {
+        if (amount > Chips)
+            throw new System.InvalidOperationException("Cannot bet more chips than available");
+        Chips -= amount;
+    }
+
+    public void WinChips(int amount)
+    {
+        Chips += amount;
+    }
+
+    public void Fold()
+    {
+        HasFolded = true;
+    }
+
+    public void NewRound()
+    {
+        Hand = new Hand();
+        HasFolded = false;
+    }
+
+    public override string ToString()
+    {
+        return $"{Name} - Chips: {Chips}";
+    }
+}
