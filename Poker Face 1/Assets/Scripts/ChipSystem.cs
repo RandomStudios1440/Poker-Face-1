9adf2fe146cf2d13a9ff3cc1998045e8730c5ac6
CDREDsauce <cameronward09092009@gmail.com>
2026-04-08 09:50:56 -0400

    s
    s

---
+++ Poker Face 1/Assets/Scripts/ChipSystem.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using TMPro;
+using System.Collections;
+using System.Collections.Generic;
+public class ChipSystem : MonoBehaviour
+{
+    [SerializeField] ChipSystem shopMenu;
+    [SerializeField] TMP_Text coinText;
+
+
+    void Update()
+    {
+        
+    }
+    
+        
+    
+}
