c177b5a4337dd77a2166d80690195eac11787e32
CDREDsauce <cameronward09092009@gmail.com>
2026-04-14 09:48:37 -0400

    s
    s

--- Poker Face 1/Assets/Scripts/ChipSystem.cs
+++ Poker Face 1/Assets/Scripts/ChipSystem.cs
@@ -1,16 +1,16 @@
-using UnityEngine;
-using TMPro;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
 public class ChipSystem : MonoBehaviour
 {
     [SerializeField] ChipSystem shopMenu;
-    [SerializeField] TMP_Text coinText;
-
+    [SerializeField] TMP_Text chipText;
+    private object chips;
 
     void Update()
     {
-        
+        chipText.text = shopMenu.chips.ToString();
     }
     
         
